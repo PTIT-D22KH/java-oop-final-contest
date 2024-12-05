5e165261df93045a886bb19e4527a20fc4a2c6a2
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-12-05 18:56:44 +0700

    add files

---
+++ java-oop-practice/src/bai1/Bai1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bai1;
+
+/**
+ *
+ * @author P51
+ */
+public class Bai1 {
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+}
